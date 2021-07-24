de330aa77ecff57fe2f7ea8832bffb6653c513c5
thienth32 <64114903+thienth32@users.noreply.github.com>
2021-07-24 10:26:40 +0700

    update code

---
+++ thi_v1/commons/helpers.php
@@ -0,0 +1,11 @@
+<?php
+
+function dd($value){
+    echo "<pre>";
+    var_dump($value);
+    die;
+}
+
+const BASE_URL = "tự sửa đường dẫn vào đây";
+
+?>
---
+++ thi_v1/index.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+require_once './vendor/autoload.php';
+require_once './commons/helpers.php';
+require_once './config/database.php';
+require_once './config/routing.php';
+
+
+?>
